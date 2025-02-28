5ab30281bfb6ba6db187077bfa39679debc21cae
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2025-02-28 11:08:32 -0800

    initial commit

---
+++ src/nocamel/converters.py
@@ -0,0 +1,14 @@
+import re
+
+
+def to_snake(input_str: str) -> str:
+    return re.sub(r"\s+", "_", input_str).lower()
+
+
+def to_lower(input_str: str) -> str:
+    return re.sub(r"(?<!^)(?=[A-Z])", "", input_str).lower()
+
+
+def to_sentence(input_str: str) -> str:
+    sentence = re.sub(r"(?<!^)(?=[A-Z])", "", input_str).lower()
+    return sentence.capitalize()
