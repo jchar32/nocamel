0371dec233c4496781154b03b4118d423e5e0e68
Jesse Charlton <57236497+jchar32@users.noreply.github.com>
2025-02-28 14:30:59 -0800

    included kebab case in functionality

--- src/nocamel/__init__.py
+++ src/nocamel/__init__.py
@@ -10,12 +10,13 @@ __version__ = "2025.1.0"
 @click.option("--snake", is_flag=True, help="Convert to snake_case")
 @click.option("--lower", is_flag=True, help="Convert to lowercase")
 @click.option("--sentence", is_flag=True, help="Convert to sentence case")
+@click.option("--kebab", is_flag=True, help="Convert to kebab case")
 @click.argument("input", type=click.STRING)
-def main(snake: bool | None, lower: bool | None, sentence: bool | None, input: str) -> None:
-    if [snake, lower, sentence].count(True) > 1:
+def main(snake: bool | None, lower: bool | None, sentence: bool | None, kebab: bool | None, input: str) -> None:
+    if [snake, lower, sentence, kebab].count(True) > 1:
         raise ValueError("Exactly one of --snake, --lower, or --sentence must be specified.")
 
-    if not any([snake, lower, sentence]):
+    if not any([snake, lower, sentence, kebab]):
         sentence = True
 
     if snake:
@@ -24,7 +25,8 @@ def main(snake: bool | None, lower: bool | None, sentence: bool | None, input: s
         result = converters.to_lower(input)
     elif sentence:
         result = converters.to_sentence(input)
-
+    elif kebab:
+        result = converters.to_kebab
     print(result)
     try:
         pyperclip.copy(result)  # Copy to clipboard
